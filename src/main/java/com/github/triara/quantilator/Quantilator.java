9991d760aabcf47fbf8843d6119ba7d5134917e1
Triara <kristina.lyubomirova@bbva.es>
2014-04-12 15:31:30 +0200

    first model of skeleton created

--- src/main/java/com/github/triara/quantilator/Quantilator.java
+++ src/main/java/com/github/triara/quantilator/Quantilator.java
@@ -1,7 +1,15 @@
 package com.github.triara.quantilator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Quantilator {
 
-  // magic calculus will be here! ;)
-
+	public static Double calculateQuantile(ArrayList<Double> list, Double quantile) {
+		list.removeAll(Collections.singleton(null));
+		if (list.size() == 0){
+			return null;
+		}
+		return 0.0;
+	}
 }
--- src/test/java/com/github/triara/quantilator/QuantilatorTest.java
+++ src/test/java/com/github/triara/quantilator/QuantilatorTest.java
@@ -1,20 +1,37 @@
 
 package com.github.triara.quantilator;
 
-import static junit.framework.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.junit.Before;
+import static junit.framework.Assert.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class QuantilatorTest {
 
-  @Test
-  public void testDoQuantil() throws Exception {
-    // assert test here!
-    // assertEquals("<p>This is the partial for view 1.</p>\n", stringWriter.toString());
-	assertEquals(2,2);
-  }
+	@Test
+	public void shouldReceiveArrayListAndReturnDoubles() {
+		ArrayList<Double> receivedList = createNewList(0.0);
+    
+		Double expectedResult = 0.0;
+		Double actualResult = Quantilator.calculateQuantile(receivedList, 0.5);
+    
+		assertEquals("Quantilator should receive an array list and return a double", expectedResult, actualResult);
+	}
 
+	@Test
+	public void shouldBeAbleToHandleNullValues() {
+		ArrayList<Double> receivedList = createNewList((Double)null);
+		
+		assertNull("If an array with one null value is received method should return another null",Quantilator.calculateQuantile(receivedList, 0.5));
+	}
+	
+	private ArrayList<Double> createNewList(Double ... values){
+		ArrayList<Double> list = new ArrayList<Double>();
+		for (Double value : values) {
+			list.add(value);
+		}
+		return list;
+		
+	}
+  
 }
