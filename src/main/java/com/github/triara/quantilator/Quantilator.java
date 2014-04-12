0636270efa505e8f94f46a00d3c3f353de37cf3b
Triara <kristina.lyubomirova@bbva.es>
2014-04-12 16:46:18 +0200

    added type 6, 7 & 8 quantile definitions

--- src/main/java/com/github/triara/quantilator/Quantilator.java
+++ src/main/java/com/github/triara/quantilator/Quantilator.java
@@ -4,12 +4,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Quantilator {
-
-	public static Double calculateQuantile(ArrayList<Double> list, Double quantile) {
+private static double m;
+private static int n;
+private static int j;
+private static double gamma;
+	
+	public static Double calculateQuantile(ArrayList<Double> list, Double quantile, Integer type) {
 		list.removeAll(Collections.singleton(null));
 		if (list.size() == 0){
 			return null;
 		}
-		return 0.0;
+		
+		Collections.sort(list);
+		n = list.size();
+		
+		if (type == 6) {
+			m = quantile;
+		}else if (type == 7) {
+			m = 1.0 - quantile;
+		}else if (type == 8) {
+			m = (quantile + 1)/3;
+		}
+		
+		
+		j = (int) (quantile*n + m);
+		gamma = quantile*n + m - j;
+		
+		int lowerElement = (j - 1 < 0) ? 0 : j - 1;
+		int upperElement = (j == n) ? j - 1 : j;
+		
+		return (((1.0 - gamma)*list.get(lowerElement)) + gamma*list.get(upperElement));
 	}
 }
--- src/test/java/com/github/triara/quantilator/QuantilatorTest.java
+++ src/test/java/com/github/triara/quantilator/QuantilatorTest.java
@@ -13,7 +13,7 @@ public class QuantilatorTest {
 		ArrayList<Double> receivedList = createNewList(0.0);
     
 		Double expectedResult = 0.0;
-		Double actualResult = Quantilator.calculateQuantile(receivedList, 0.5);
+		Double actualResult = Quantilator.calculateQuantile(receivedList, 0.5, 8);
     
 		assertEquals("Quantilator should receive an array list and return a double", expectedResult, actualResult);
 	}
@@ -22,7 +22,37 @@ public class QuantilatorTest {
 	public void shouldBeAbleToHandleNullValues() {
 		ArrayList<Double> receivedList = createNewList((Double)null);
 		
-		assertNull("If an array with one null value is received method should return another null",Quantilator.calculateQuantile(receivedList, 0.5));
+		assertNull("If an array with one null value is received method should return another null",Quantilator.calculateQuantile(receivedList, 0.5, 8));
+	}
+	
+	@Test
+	public void shouldCalculatePercentilesForType8Definition() {
+		ArrayList<Double> receivedList = createNewList(10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,100.0);
+		
+		Double expectedResult = 82.9;
+		Double actualResult = Quantilator.calculateQuantile(receivedList, 0.77, 8);
+		
+		assertEquals("Should calculate quantile for type 8 definition", expectedResult, actualResult);
+	}
+	
+	@Test
+	public void shouldCalculatePercentilesForType7Definition() {
+		ArrayList<Double> receivedList = createNewList(10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,100.0);
+		
+		Double expectedResult = 79.3;
+		Double actualResult = Quantilator.calculateQuantile(receivedList, 0.77, 7);
+		
+		assertEquals("Should calculate quantile for type 7 definition", expectedResult, actualResult);
+	}
+	
+	@Test
+	public void shouldCalculatePercentilesForType6Definition() {
+		ArrayList<Double> receivedList = createNewList(10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,100.0);
+		
+		Double expectedResult = 84.7;
+		Double actualResult = Quantilator.calculateQuantile(receivedList, 0.77, 6);
+		
+		assertEquals("Should calculate quantile for type 6 definition", expectedResult, actualResult);
 	}
 	
 	private ArrayList<Double> createNewList(Double ... values){
