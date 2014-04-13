896488eb520139ae849a661ce1e7024460083c50
Triara <kristina.lyubomirova@bbva.es>
2014-04-13 09:48:26 +0200

    refactor calculateQuantile method

--- src/main/java/com/github/triara/quantilator/Quantilator.java
+++ src/main/java/com/github/triara/quantilator/Quantilator.java
@@ -11,50 +11,48 @@ private static double gamma;
 	
 	public static Double calculateQuantile(ArrayList<Double> list, Double quantile, Integer type) {
 		list.removeAll(Collections.singleton(null));
-		if (list.size() == 0){
-			return null;
-		}
-		
+		if (list.size() == 0) return null;		
 		Collections.sort(list);
+		
 		n = list.size();
-		
-		if (type == 1) {
-			m = 0.0;
-		}else if (type == 2){
-			m = 0.0;
-		}else if (type == 3) {
-			m = -0.5;
-		}else if (type == 4) {
-			m = 0.0;
-		}else if (type == 5) {
-			m = 0.5;
-		}else if (type == 6) {
-			m = quantile;
-		}else if (type == 7) {
-			m = 1.0 - quantile;
-		}else if (type == 8) {
-			m = (quantile + 1)/3;
-		}else if (type == 9) {
-			m = 0.25*quantile + 0.375;
-		}
-		
-		
+		m = calculateAuxiliarConstantM(quantile, type);
 		j = (int) (quantile*n + m);
-		
-		if (type == 1) {
-			gamma = ((quantile*n + m - j) > 0)? 1.0 : 0.0;
-		}else if (type == 2) {
-			gamma = ((quantile*n + m - j) > 0)? 1.0 : 0.5;
-		}else if(type == 3) {
-			gamma = ((quantile*n + m - j) == 0 && isEven(j))? 0.0 : 1.0;
-		}else {
-			gamma = quantile*n + m - j;			
-		}
+		gamma = calculateAuxiliarConstantGamma(quantile, type);
 		
 		int lowerElement = (j - 1 < 0) ? 0 : j - 1;
 		int upperElement = (j == n) ? j - 1 : j;
 		
 		return (((1.0 - gamma)*list.get(lowerElement)) + gamma*list.get(upperElement));
+	}
+
+	private static double calculateAuxiliarConstantGamma(Double quantile, Integer type) {
+		if (type == 1) {
+			return ((quantile*n + m - j) > 0)? 1.0 : 0.0;
+		}else if (type == 2) {
+			return ((quantile*n + m - j) > 0)? 1.0 : 0.5;
+		}else if(type == 3) {
+			return ((quantile*n + m - j) == 0 && isEven(j))? 0.0 : 1.0;
+		}else {
+			return quantile*n + m - j;			
+		}
+	}
+
+	private static double calculateAuxiliarConstantM(Double quantile, Integer type) {
+		if (type == 1 || type == 2 || type == 4) {
+			return 0.0;
+		}else if (type == 3) {
+			return -0.5;
+		}else if (type == 5) {
+			return 0.5;
+		}else if (type == 6) {
+			return quantile;
+		}else if (type == 7) {
+			return 1.0 - quantile;
+		}else if (type == 9) {
+			return 0.25*quantile + 0.375;
+		}else {
+			return (quantile + 1)/3;
+		}
 	}
 	
 	public static boolean isEven(int number){
@@ -63,5 +61,4 @@ private static double gamma;
 		}
 		return false;
 	}
-	
 }
